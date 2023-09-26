ae37f10d7e4c9df7942e9a14af98d623d6f1c167
sthefaniafs <sthefaniafernandes03@gmail.com>
2023-09-26 13:47:09 -0300

    update ex3

--- ex3/senoide.cpp
+++ ex3/senoide.cpp
@@ -7,22 +7,21 @@ int SIDE = 256;
 int PERIODOS = 4;
 
 int main(int argc, char** argv) {
-  std::stringstream ss_img, ss_yml;
-  cv::Mat image;
+    std::stringstream ss_img, ss_yml;
+    cv::Mat image;
 
-  ss_yml << "senoide-" << SIDE << ".yml";
-  image = cv::Mat::zeros(SIDE, SIDE, CV_32FC1);
+    ss_yml << "senoide-" << SIDE << ".yml";
+    image = cv::Mat::zeros(SIDE, SIDE, CV_32FC1);
 
-  cv::FileStorage fs(ss_yml.str(), cv::FileStorage::WRITE);
+    cv::FileStorage fs(ss_yml.str(), cv::FileStorage::WRITE);
 
-  for (int i = 0; i < SIDE; i++) {
-    for (int j = 0; j < SIDE; j++) {
-      image.at<float>(i, j) = 127 * sin(2 * M_PI * PERIODOS * j / SIDE) + 128;
+    for (int i = 0; i < SIDE; i++) {
+      for (int j = 0; j < SIDE; j++) {
+        image.at<float>(i, j) = 127 * sin(2 * M_PI * PERIODOS * j / SIDE) + 128;
+      }
     }
-  }
     /*arquivo txt da imagem yml, será coletada uma linha para comparação com png*/
     std::ofstream Fileyml("img_yml.txt");
-    
 
     if (!Fileyml.is_open()){
         std::cout << "Erro ao criar o arquivo .txt" << std::endl;
@@ -35,19 +34,19 @@ int main(int argc, char** argv) {
     }
     Fileyml.close();
 
-  fs << "mat" << image;
-  fs.release();
+    fs << "mat" << image;
+    fs.release();
 
-  cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
-  image.convertTo(image, CV_8U);
-  ss_img << "senoide-" << SIDE << ".png";
-  cv::imwrite(ss_img.str(), image);
+    cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
+    image.convertTo(image, CV_8U);
+    ss_img << "senoide-" << SIDE << ".png";
+    cv::imwrite(ss_img.str(), image);
 
-  fs.open(ss_yml.str(), cv::FileStorage::READ);
-  fs["mat"] >> image;
+    fs.open(ss_yml.str(), cv::FileStorage::READ);
+    fs["mat"] >> image;
 
-  cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
-  image.convertTo(image, CV_8U);
+    cv::normalize(image, image, 0, 255, cv::NORM_MINMAX);
+    image.convertTo(image, CV_8U);
 
     /*arquivo txt da imagem png, será coletada uma linha para comparação com yml*/
     std::ofstream Filepng("img_png.txt");
@@ -64,8 +63,8 @@ int main(int argc, char** argv) {
         }
         Filepng.close();
 
-  cv::imshow("image", image);
-  cv::waitKey();
+    cv::imshow("image", image);
+    cv::waitKey();
 
-  return 0;
+    return 0;
 }
